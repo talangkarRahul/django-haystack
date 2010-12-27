7579f40ab4f62cb0aa41fcadb6a8e806f3749676
Chris Adams <chris@improbable.org>
2010-12-27 13:55:20 -0500

    Tests: fix test_load_nonexistent test
    Previously the exception hard-coded a list of things which was setting-dependent.

--- tests/core/tests/backends.py
+++ tests/core/tests/backends.py
@@ -34,8 +34,4 @@ class LoadBackendTestCase(TestCase):
         self.assertEqual(backend.BACKEND_NAME, 'simple')
     
     def test_load_nonexistent(self):
-        try:
-            backend = haystack.load_backend('foobar')
-            self.fail()
-        except ImproperlyConfigured, e:
-            self.assertEqual(str(e), "'foobar' isn't an available search backend. Available options are: 'dummy', 'simple', 'solr', 'whoosh'")
+        self.failUnlessRaises(ImproperlyConfigured, haystack.load_backend, ('foobar',))
